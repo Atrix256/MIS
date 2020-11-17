42ff9abf593f2cb528e8e2bda37b10b65fde3ec9
Alan Wolfe <alan.wolfe@gmail.com>
2020-11-16 20:40:45 -0800

    start

---
+++ main.cpp
@@ -0,0 +1,22 @@
+int main(int argc, char** argv)
+{
+    return 0;
+}
+
+/*
+
+TODO:
+* sample 2 functions multiplied together - compare to sampling one, or the other, or uniform
+* stochastically? with LDS?
+* show the results
+
+Blog:
+* real simple / light on math / plainly described.
+* a follow up to 1d monte carlo / importance sampling:
+ * https://blog.demofox.org/2018/06/12/monte-carlo-integration-explanation-in-1d/
+
+Links:
+https://64.github.io/multiple-importance-sampling/
+https://www.breakin.se/mc-intro/index.html#toc3
+
+*/
