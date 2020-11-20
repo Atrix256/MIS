c620bd49a31d8e9d27439e067d7fcbd7bbe32ec7
Alan Wolfe <alan.wolfe@gmail.com>
2020-11-19 16:18:06 -0800

    mis begin

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <random>
 #include <vector>
 
-#define DETERMINISTIC() false
+#define DETERMINISTIC() true
 
 static const size_t c_numSamples = 500; // TODO: larger sample count?
 
@@ -195,14 +195,50 @@ double ImportanceSampledMonteCarloBlue(const TF& F, const TPDF& PDF, const TINVE
     return estimate;
 }
 
+template <typename TF, typename TPDF1, typename TINVERSECDF1, typename TPDF2, typename TINVERSECDF2>
+double MultipleImportanceSampledMonteCarlo(const TF& F, const TPDF1& PDF1, const TINVERSECDF1& InverseCDF1, const TPDF2& PDF2, const TINVERSECDF2& InverseCDF2, std::vector<double>& estimates)
+{
+    std::mt19937 rng = GetRNG();
+    std::uniform_real_distribution<double> dist(0.0, 1.0);
+
+    estimates.resize(c_numSamples);
+
+    double estimate = 0.0f;
+    for (size_t i = 0; i < c_numSamples; ++i)
+    {
+        double x1 = InverseCDF1(dist(rng));
+        double y1 = F(x1);
+        double pdf1 = PDF1(x1);
+        double value1 = y1 / pdf1;
+
+        double x2 = InverseCDF2(dist(rng));
+        double y2 = F(x2);
+        double pdf2 = PDF2(x2);
+        double value2 = y2 / pdf2;
+
+        double weight1 = pdf1 / (pdf1 + pdf2);
+        double weight2 = pdf2 / (pdf1 + pdf2);
+
+        double value = value1 * weight1 + value2 * weight2;
+
+        AddSampleToRunningAverage(estimate, value, i);
+        estimates[i] = estimate;
+    }
+
+    return estimate;
+}
+
 int main(int argc, char** argv)
 {
+    // y = sin(x)*sin(x) from 0 to pi
     {
+        // The function we are integrating
         auto F = [](double x) -> double
         {
             return sin(x) * sin(x);
         };
 
+        // the PDF and inverse CDF of a distribution we are using for integration
         auto PDF = [](double x) -> double
         {
             return sin(x) / 2.0f;
@@ -236,15 +272,19 @@ int main(int argc, char** argv)
 
         // report results
         {
-            printf("mc       = %f  (%f)\n", mc, abs(mc - c_actual));
-            printf("mcblue   = %f  (%f)\n", mcblue, abs(mcblue - c_actual));
-            printf("mclds    = %f  (%f)\n", mclds, abs(mclds - c_actual));
-            printf("ismc     = %f  (%f)\n", ismc, abs(ismc - c_actual));
-            printf("ismcblue = %f  (%f)\n", ismcblue, abs(ismcblue - c_actual));
-            printf("ismclds  = %f  (%f)\n", ismclds, abs(ismclds - c_actual));
+            // summary to screen
+            printf("y = sin(x)*sin(x) from 0 to pi\n");
+            printf("  mc       = %f  (%f)\n", mc, abs(mc - c_actual));
+            printf("  mcblue   = %f  (%f)\n", mcblue, abs(mcblue - c_actual));
+            printf("  mclds    = %f  (%f)\n", mclds, abs(mclds - c_actual));
+            printf("  ismc     = %f  (%f)\n", ismc, abs(ismc - c_actual));
+            printf("  ismcblue = %f  (%f)\n", ismcblue, abs(ismcblue - c_actual));
+            printf("  ismclds  = %f  (%f)\n", ismclds, abs(ismclds - c_actual));
+            printf("\n");
 
+            // details to csv
             FILE* file = nullptr;
-            fopen_s(&file, "out.csv", "wb");
+            fopen_s(&file, "out1.csv", "wb");
             fprintf(file, "\"index\",\"mc\",\"mcblue\",\"mclds\",\"ismc\",\"ismcblue\",\"ismclds\"\n");
             for (size_t i = 0; i < c_numSamples; ++i)
             {
@@ -259,6 +299,115 @@ int main(int argc, char** argv)
             fclose(file);
         }
     }
+
+    // y=sin(x)*2x from 0 to pi
+    {
+        // The function we are integrating
+        auto F = [](double x) -> double
+        {
+            return sin(x) * 2.0f * x;
+        };
+
+        // the PDF and inverse CDF of distributions we are using for integration
+        auto PDF1 = [](double x) -> double
+        {
+            // normalizing y=sin(x) from 0 to pi to integrate to 1
+            return sin(x) / 2.0f;
+        };
+
+        auto InverseCDF1 = [](double x) -> double
+        {
+            // turning the PDF into a CDF, flipping x and y, and solving for y again
+            return 2.0 * asin(sqrt(x));
+        };
+
+        auto PDF2 = [](double x) -> double
+        {
+            // normalizing y=2x from 0 to pi to integrate to 1
+            return x * 2.0f / (c_pi * c_pi);
+        };
+
+        auto InverseCDF2 = [](double x) -> double
+        {
+            // turning the PDF into a CDF, flipping x and y, and solving for y again
+            return c_pi * sqrt(x);
+        };
+
+        double c_actual = 2.0f * c_pi;
+
+        // numerical integration
+        std::vector<double> mcEstimates;
+        double mc = MonteCarlo(F, mcEstimates);
+
+        std::vector<double> mcblueEstimates;
+        double mcblue = MonteCarloBlue(F, mcblueEstimates);
+
+        std::vector<double> mcldsEstimates;
+        double mclds = MonteCarloLDS(F, mcldsEstimates);
+
+        std::vector<double> ismc1Estimates;
+        double ismc1 = ImportanceSampledMonteCarlo(F, PDF1, InverseCDF1, ismc1Estimates);
+
+        std::vector<double> ismcblue1Estimates;
+        double ismcblue1 = ImportanceSampledMonteCarloBlue(F, PDF1, InverseCDF1, ismcblue1Estimates);
+
+        std::vector<double> ismclds1Estimates;
+        double ismclds1 = ImportanceSampledMonteCarloLDS(F, PDF1, InverseCDF1, ismclds1Estimates);
+
+        std::vector<double> ismc2Estimates;
+        double ismc2 = ImportanceSampledMonteCarlo(F, PDF2, InverseCDF2, ismc2Estimates);
+
+        std::vector<double> ismcblue2Estimates;
+        double ismcblue2 = ImportanceSampledMonteCarloBlue(F, PDF2, InverseCDF2, ismcblue2Estimates);
+
+        std::vector<double> ismclds2Estimates;
+        double ismclds2 = ImportanceSampledMonteCarloLDS(F, PDF2, InverseCDF2, ismclds2Estimates);
+
+        std::vector<double> mismcEstimates;
+        double mismc = MultipleImportanceSampledMonteCarlo(F, PDF1, InverseCDF1, PDF2, InverseCDF2, mismcEstimates);
+
+        // report results
+        {
+            // summary to screen
+            printf("y=sin(x)*2x from 0 to pi\n");
+            printf("  mc        = %f  (%f)\n", mc, abs(mc - c_actual));
+            printf("  mcblue    = %f  (%f)\n", mcblue, abs(mcblue - c_actual));
+            printf("  mclds     = %f  (%f)\n", mclds, abs(mclds - c_actual));
+            printf("  ismc1     = %f  (%f)\n", ismc1, abs(ismc1 - c_actual));
+            printf("  ismcblue1 = %f  (%f)\n", ismcblue1, abs(ismcblue1 - c_actual));
+            printf("  ismclds1  = %f  (%f)\n", ismclds1, abs(ismclds1 - c_actual));
+            printf("  ismc2     = %f  (%f)\n", ismc2, abs(ismc2 - c_actual));
+            printf("  ismcblue2 = %f  (%f)\n", ismcblue2, abs(ismcblue2 - c_actual));
+            printf("  ismclds2  = %f  (%f)\n", ismclds2, abs(ismclds2 - c_actual));
+            printf("  mismc     = %f  (%f)\n", mismc, abs(mismc - c_actual));
+            printf("\n");
+
+            // details to csv
+            FILE* file = nullptr;
+            fopen_s(&file, "out2.csv", "wb");
+            fprintf(file, "\"index\",\"mc\",\"mcblue\",\"mclds\",\"ismc1\",\"ismcblue1\",\"ismclds1\",\"ismc2\",\"ismcblue2\",\"ismclds2\",\"mismc\"\n");
+            for (size_t i = 0; i < c_numSamples; ++i)
+            {
+                fprintf(file, "\"%zu\",", i);
+                fprintf(file, "\"%f\",", max(abs(mcEstimates[i] - c_actual), c_minError));
+                fprintf(file, "\"%f\",", max(abs(mcblueEstimates[i] - c_actual), c_minError));
+                fprintf(file, "\"%f\",", max(abs(mcldsEstimates[i] - c_actual), c_minError));
+                fprintf(file, "\"%f\",", max(abs(ismc1Estimates[i] - c_actual), c_minError));
+                fprintf(file, "\"%f\",", max(abs(ismcblue1Estimates[i] - c_actual), c_minError));
+                fprintf(file, "\"%f\",", max(abs(ismclds1Estimates[i] - c_actual), c_minError));
+                fprintf(file, "\"%f\",", max(abs(ismc2Estimates[i] - c_actual), c_minError));
+                fprintf(file, "\"%f\",", max(abs(ismcblue2Estimates[i] - c_actual), c_minError));
+                fprintf(file, "\"%f\",", max(abs(ismclds2Estimates[i] - c_actual), c_minError));
+                fprintf(file, "\"%f\"\n", max(abs(mismcEstimates[i] - c_actual), c_minError));
+            }
+            fclose(file);
+        }
+    }
+
+    // TODO: MIS in 2nd test doesn't seem to be working correctly! more error than IS's individually.
+    // TODO: should 2nd test have LDS and Blue noise versions? maybe so... could be interesting!
+    // TODO: do a 3rd test where it's a second test multiplied by some boolean function (visibility)
+
     return 0;
 }
 
@@ -284,5 +433,6 @@ Blog:
 Links:
 https://64.github.io/multiple-importance-sampling/
 https://www.breakin.se/mc-intro/index.html#toc3
+https://graphics.stanford.edu/courses/cs348b-03/papers/veach-chapter9.pdf
 
 */
